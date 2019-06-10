a84da21878610fdde4a8d4cfabaa9568282da2c4
hheiling <hheiling@live.unc.edu>
2019-06-10 13:30:07 -0400

    Added formulaData function - deals with data = data.frame or data = NULL

--- R/glmmPen.R
+++ R/glmmPen.R
@@ -1,7 +1,6 @@
 #' @importFrom stringr str_to_lower
-#' @importFrom lme4 mkReTrms nobars subbars findbars
 #' @export
-glmmPen = function(formula, data, family = "binomial", na.action = na.omit,
+glmmPen = function(formula, data = NULL, family = "binomial", na.action = na.omit,
                   offset = NULL, weights = NULL, penalty = "grMCP",
                   lambda0 = 0, lambda1 = 0, nMC = 100, nMC_max = 2000, returnMC = T,
                   maxitEM = 100, trace = 0, conv = 0.001, 
@@ -40,88 +39,27 @@ glmmPen = function(formula, data, family = "binomial", na.action = na.omit,
   }
 
   # Convert formula and data to useful forms to plug into fit_dat
+  fD_out = formulaData(formula, data)
   
-  ## substitute | for +
-  form_full = subbars(formula)
-  
-  # Deal with NAs
-  if(na.action == na.omit){ # Need to use character? Is na.action a function? ...
-    data = na.omit(data[,colnames(model.frame(form_full, data = data))])
-  }else{
-    warning("This function not equipted to deal with NA values. \n
-            Please check that data does not contain NA values. \n", immediate. = T)
-  }
-  
-  ## Add offset = offset, weights = weights in model.frame function?
-  frame_full = model.frame(form_full, data = data)
-  
-  ## Identify random effects
-  ### If no | (no random effects listed) then stop - mkBlist called by mkReTrms give this error
-  reExprs = findbars(formula)
-  reTrms = mkReTrms(reExprs, frame_full)
-  # t(Zt) from mkReTrms: columns organized by group level, then vars within group level
-  Zt = reTrms$Zt
-  
-  # Change group condition below?
-  if(length(reTrms$flist) > 1){
-    stop("procedure can only handle one group")
-  }else{
-    group = reTrms$flist[[1]]
-    group_name = names(reTrms$flist)
-  }
-  
-  d = nlevels(group[[1]])
-  Z = Matrix(0, nrow = ncol(Zt), ncol = nrow(Zt), sparse = T)
-  # Want Z columns organized by vars, then levels of group within vars
-  for(lev in 1:d){
-    Z[,(d*(lev-1)+1):(d*lev)] = Matrix::t(Zt[seq(lev, by = d, length.out = nrow(Zt)/d),])
-  }
-  # Z_dense = Matrix::as.matrix(Z) # Convert Z to dense matrix
-  
-  ## Get fixed effects X matrix
-  formula_nobars = nobars(formula)
-  X = model.matrix(formula_nobars, data)
-  
-  # Problem if variable specified in formula is the intercept / a constant column 
-  # and -1 not included in formula
-  constant_cols = X[,apply(X, 2, var, na.rm=TRUE) == 0]
-  ## Change to matrix (X not data.frame)
-  if(class(constant_cols) == "matrix"){ # If true, more than one column with zero variance
-    stop("Variable(s) in formula has zero variance (constant column).
-         Either remove this variable from formula or specify -1 in formula")
-  } # If only one column, class(constant_cols) == "numeric"
-
-  ## Make sure colnames random effects subset of colnames X
-  cnms = reTrms$cnms[[1]]
-  if(sum(!(cnms %in% colnames(X))) > 0){
-    stop("random effects must be a subset of fixed effects")
+  ## Convert group to numeric factor - for fit_dat
+  if(any(is.character(group))){
+    group_num = as.factor(as.numeric(fD_out$group))
   }
 
-  ## Creating response variable Y
-  Y = model.response(frame_full)
-  ### Other options if including offset and/or weights?
-  ### Use mkRespMod if offset / weights used
-  if(is.null(offset) && is.null(weights)){
-    Y = model.response(frame_full)
-  }else{
-    # This currently doesn't work; need to experiment with and fix
-    # Y = lme4::mkRespMod(frame_full, family = family, y = model.response(frame_full),
-    #               offset = offset, weights = weights)
-    stop("glmmPen function currently not enabled to use offset and weight parameters")
-  }
-
-  data_input = list(y = Y, X = X, Z = Z, group = group)
+  data_input = list(y = fD_out$y, X = fD_out$X, Z = fD_out$Z, group = group_num)
   
-  coef_names = list(fixed = colnames(X), random = cnms, group = group_name)
+  coef_names = list(fixed = colnames(fD_out$X), random = fD_out$cnms, group = fD_out$group_name)
   
   # Things that should be included in call:
   ## formula, data, any other items included in glmmPen function call
   call = match.call(expand.dots = F)
+  
+  
 
   # Call fit_dat function - adjust to use match.call object?
   # fit_dat object found in "/R/fit_dat.R" file
   fit = fit_dat(dat = data_input, lambda0 = lambda0, lambda1 = lambda1, nMC = nMC,
-                   family = family, group = group, trace = trace, penalty = penalty,
+                   family = family, group = group_num, trace = trace, penalty = penalty,
                    conv = conv, nMC_max = nMC_max, returnMC = returnMC, gibbs = T,
                    maxitEM = maxitEM, alpha = alpha)
   
@@ -139,3 +77,72 @@ glmmPen = function(formula, data, family = "binomial", na.action = na.omit,
 
 }
 
+# ## substitute | for +
+# form_full = subbars(formula)
+# 
+# # Deal with NAs
+# if(na.action == na.omit){ # Need to use character? Is na.action a function? ...
+#   data = na.omit(data[,colnames(model.frame(form_full, data = data))])
+# }else{
+#   warning("This function not equipted to deal with NA values. \n
+#           Please check that data does not contain NA values. \n", immediate. = T)
+# }
+# 
+# ## Add offset = offset, weights = weights in model.frame function?
+# frame_full = model.frame(form_full, data = data)
+# 
+# ## Identify random effects
+# ### If no | (no random effects listed) then stop - mkBlist called by mkReTrms give this error
+# reExprs = findbars(formula)
+# reTrms = mkReTrms(reExprs, frame_full)
+# # t(Zt) from mkReTrms: columns organized by group level, then vars within group level
+# Zt = reTrms$Zt
+# 
+# # Change group condition below?
+# if(length(reTrms$flist) > 1){
+#   stop("procedure can only handle one group")
+# }else{
+#   group = reTrms$flist[[1]]
+#   group_name = names(reTrms$flist)
+# }
+# 
+# d = nlevels(group[[1]])
+# Z = Matrix(0, nrow = ncol(Zt), ncol = nrow(Zt), sparse = T)
+# # Want Z columns organized by vars, then levels of group within vars
+# for(lev in 1:d){
+#   Z[,(d*(lev-1)+1):(d*lev)] = Matrix::t(Zt[seq(lev, by = d, length.out = nrow(Zt)/d),])
+# }
+# # Z_dense = Matrix::as.matrix(Z) # Convert Z to dense matrix
+# 
+# ## Get fixed effects X matrix
+# formula_nobars = nobars(formula)
+# X = model.matrix(formula_nobars, data)
+# 
+# # Problem if variable specified in formula is the intercept / a constant column 
+# # and -1 not included in formula
+# constant_cols = X[,apply(X, 2, var, na.rm=TRUE) == 0]
+# ## Change to matrix (X not data.frame)
+# if(class(constant_cols) == "matrix"){ # If true, more than one column with zero variance
+#   stop("Variable(s) in formula has zero variance (constant column).
+#        Either remove this variable from formula or specify -1 in formula")
+# } # If only one column, class(constant_cols) == "numeric"
+# 
+# ## Make sure colnames random effects subset of colnames X
+# cnms = reTrms$cnms[[1]]
+# if(sum(!(cnms %in% colnames(X))) > 0){
+#   stop("random effects must be a subset of fixed effects")
+# }
+# 
+# ## Creating response variable Y
+# Y = model.response(frame_full)
+# ### Other options if including offset and/or weights?
+# ### Use mkRespMod if offset / weights used
+# if(is.null(offset) && is.null(weights)){
+#   Y = model.response(frame_full)
+# }else{
+#   # This currently doesn't work; need to experiment with and fix
+#   # Y = lme4::mkRespMod(frame_full, family = family, y = model.response(frame_full),
+#   #               offset = offset, weights = weights)
+#   stop("glmmPen function currently not enabled to use offset and weight parameters")
+# }
+# 
