2732d63121bc406f98d26e8700f93ea305b08ff1
hheiling <hheiling@live.unc.edu>
2019-07-10 14:14:51 -0400

    Updated with summary and plot

--- R/object_methods.R
+++ R/object_methods.R
@@ -347,7 +347,7 @@ prt_ranef = function(object, digits = 4){
 }
 
 prt_nobsgrps = function(object){
-  cat(sprintf("Number Observations: %i,  groups: %s, %i", 
+  cat(sprintf("Number Observations: %i,  groups: %s, %i \n", 
               nobs(object), names(object$group), nlevels(object$group[[1]])))
 }
 
@@ -373,8 +373,64 @@ print.pglmmObj = function(object, digits = c(fef = 4, ref = 4)){
   invisible(object)
 }
 
+summary_ranef = function(object, digits = 4){
+  cat("Random Effects:\n")
+  # Create character matrix
+  cnms = c("Group","Name","Variance","Std.Dev.")
+  # Assume only one group
+  group = object$group[[1]] 
+  group_name = names(object$group)
+  ref = lapply(ranef(object), function(x) colnames(x))
+  ref_names = ref[[1]]
+  sigma = object$sigma
+  output = matrix(0, nrow = length(ref_names), ncol = length(cnms))
+  output[,2] = ref_names
+  output[,1] = group_name
+  output[,3] = round(diag(sigma), digits = digits)
+  output[,4] = round(sqrt(diag(sigma)), digits = digits)
+  colnames(output) = cnms
+  rownames(output) = rep("", nrow(output))
+  
+  print(output, quote = F)
+}
+
+#' @importFrom stringr str_to_title
+prt_resids = function(resids, type = "Pearson", digits) {
+  cat(str_to_title(type), "residuals: ", "\n", sep = " ")
+  
+  resid_quant <- setNames(zapsmall(quantile(resids, na.rm=TRUE), digits + 1L),
+                          c("Min", "1Q", "Median", "3Q", "Max"))
+  print(resid_quant, digits = digits)
+  cat("\n")
+}
+
+#' @export
+summary.pglmmObj = function(object, digits = c(fef = 4, ref = 4), resid_type = "deviance"){
+  # ToDo: Add in (best) lambda values, BIC, logLik
+  
+  # Title
+  cat("Penalized generalized linear mixed model fit by Monte Carlo Expectation Conditional Minimization (MCECM)",
+      "  algorithm", " (", object$sampling, ") ", " ['", class(object), "'] ", fill = T, sep = "")
+  # Family
+  prt_family(object)
+  # Call information: formula, data, weights, offset, (subset?)
+  prt_call(object)
+  cat("\n")
+  # Fixed effects information
+  prt_fixef(object, digits = digits[1])
+  cat("\n")
+  # Random Effects information
+  summary_ranef(object, digits = digits[2])
+  # Number obs and groups
+  prt_nobsgrps(object)
+  cat("\n")
+  # Residuals Summary
+  prt_resids(residuals(object, type = resid_type), type = resid_type, digits = digits[1])
+  cat("\n")
+}
+
 #' @importFrom reshape2 melt
-#' @importFrom stringr str_c str_detect
+#' @importFrom stringr str_c str_detect str_sub str_remove str_locate
 #' @export 
 plot_mcmc.pglmmObj = function(object, plots = c("all","sample.path","histogram","cumsum","autocorr"),
                      grps = "all", vars = "all", numeric.grps = F){
@@ -400,6 +456,28 @@ plot_mcmc.pglmmObj = function(object, plots = c("all","sample.path","histogram",
   U = object$gibbs_mcmc
   U_cols = colnames(U)
   # colnames organization = var_name:grp_name
+  
+  if(vars == "all"){
+    d = nlevels(object$group[[1]])
+    non0 = (diag(object$sigma) != 0)
+    non0cols = rep(non0, each = d)
+    U_non0 = U[,non0cols]
+    if(ncol(U_non0) != ncol(U)){
+      vars_non0 = str_sub(colnames(U_non0), start = 1, 
+                          end = (str_locate(colnames(U_non0), ":")[,1]-1))
+      vars_long = str_remove(str_remove(vars_non0, "[(]"), "[)]")
+      vars_string = vars_long[seq(from = 1, by = d, length.out = length(vars_long) / d)]
+      # Create reduced U (U_red)
+      for(v in vars_string){
+        if(vars_string[1] == v){
+          U_red = U[,str_detect(U_cols, str_c("[(]?",v,"[)]?",":"))]
+        }else{
+          U_red = cbind(U_red, U[,str_detect(U_cols, str_c("[(]?",v,"[)]?",":"))])
+        }
+      }
+      U = U_red
+    }
+  }
   
   # If include intercept, convert to recognized form "Intercept"
   if(vars != "all"){
@@ -551,8 +629,34 @@ plot_mcmc.pglmmObj = function(object, plots = c("all","sample.path","histogram",
   
 }
 
-# # @exportMethod show
-# setMethod("show",  "pglmmObj", function(object) print.pglmmObj(object))
+#' @export
+plot.pglmmObj = function(object, x = fitted(object), y = residuals(object, type = "deviance"),
+                      x_label = "Fitted Values", y_label = NULL){
+  
+  if(length(x) != length(y)){
+    stop("x and y need to be of same length")
+  }
+  
+  x_mat = Matrix::as.matrix(x)
+  y_mat = Matrix::as.matrix(y)
+  
+  if(is.null(y_label)){
+    if(!is.null(attr(y, "residual type"))){
+      y_label = str_c(str_to_title(attr(y, "residual type")), "Residuals", sep = " ")
+    }else{
+      y_label = "specify y_label in args"
+    }
+  }
+  data = data.frame(x = x_mat, y = y_mat)
+  
+  p = ggplot(data = data) + geom_point(mapping = aes(x = x, y = y), color = "blue") +
+    ylab(y_label) + xlab(x_label) 
+  if(!is.null(attr(y, "residual type"))){
+    p = p + geom_hline(yintercept = 0, color = "black")
+  }
+  return(p)
+}
+
 
 # Functions to add:
 ## offset() - will not have $offset option
