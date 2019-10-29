f32258de42bcfa57e77dce116e91da29b43d078c
hheiling <hheiling@live.unc.edu>
2019-10-29 09:46:27 -0400

    Updated gibbs sampler to be adaptive (update proposal var to try and get acceptance rate closer to ideal 0.44)

--- R/sample_mc_functions.R
+++ R/sample_mc_functions.R
@@ -106,7 +106,8 @@ sample.mc2 = function(fit, cov, y, X, Z, nMC, trace = 0, family = family, group,
         
         gibbs_list = sample_mc_inner_gibbs(matrix(fitted_mat[select], ncol = 1, nrow = sum(select)), 
                                            matrix(Z[select,index],ncol = length(index), nrow = sum(select)),  
-                                           y[select], uhat[index], nMC, as.numeric((uold[nrow(uold),index, drop = FALSE])), trace)
+                                           y[select], uhat[index], nMC, as.numeric((uold[nrow(uold),index, drop = FALSE])),
+                                           trace)
         u0[,index] = gibbs_list$u
         gibbs_accept_rate[i,] = matrix(gibbs_list$acc_rate, nrow = 1)
       }
@@ -122,7 +123,8 @@ sample.mc2 = function(fit, cov, y, X, Z, nMC, trace = 0, family = family, group,
       
       gibbs_list = sample_mc_inner_gibbs(matrix(fitted_mat[select], ncol = 1, nrow = sum(select)), 
                                          matrix(Z[select,index],ncol = length(index), nrow = sum(select)),  
-                                         y[select], uhat[index], nMC, as.numeric((uold[nrow(uold),index, drop = FALSE])), trace)
+                                         y[select], uhat[index], nMC, as.numeric((uold[nrow(uold),index, drop = FALSE])), 
+                                         trace)
       u0[,index] = gibbs_list$u
       gibbs_accept_rate[i,] = matrix(gibbs_list$acc_rate, nrow = 1)
     }
@@ -139,6 +141,135 @@ sample.mc2 = function(fit, cov, y, X, Z, nMC, trace = 0, family = family, group,
     return(list(u0 = u0, gibbs_accept_rate = gibbs_accept_rate, switch = error_out))
   }else{
     return(list(u0 = u0, switch = error_out))
+  }
+  
+}
+
+##################################################################
+# Updated for adaptive metropolis w/in gibbs
+##################################################################
+
+#' @export
+sample.mc3 = function(fit, cov, y, X, Z, nMC, trace = 0, family = family, group, d, nZ, okindex,
+                      gibbs = F , uold, proposal_var, batch){
+  
+  f = get(family, mode = "function", envir = parent.frame())
+  
+  # find tau for rejection sampler (Booth 1999) by first maximizing u
+  if(length(okindex) > 0 & gibbs == F){
+    fitu = glm(y ~ Z[,okindex]-1, offset = (X %*% fit$coef[1:ncol(X)]), family = f)
+    uhat = fitu$coef
+    uhat[is.na(uhat)] = 0
+    eta = X %*% fit$coef[1:ncol(X)] + Z[,okindex] %*% uhat
+  }else{
+    uhat  = rep(0, ncol(Z))
+    eta = X %*% fit$coef[1:ncol(X)] 
+  }
+  
+  #if(trace == 1) print(uhat)
+  # calculate tau
+  
+  if(family == "binomial" & gibbs == F){
+    tau = dbinom(y, size = 1, prob = exp(eta)/(1+exp(eta)), log = T)
+  }else if(family == "poisson" & gibbs == F){
+    tau = dpois(y, lambda = exp(eta), log = T)
+  }
+  
+  #if(any(tau == 0)) tau = tau - 10^-20
+  # matrix to hold accepted samples
+  u0 = matrix(rnorm(nMC*ncol(Z)) , nMC, ncol(Z))
+  #u0 = Matrix(0 , nMC, ncol(Z), sparse = T)
+  
+  # fitted
+  fitted_mat = as.matrix(X %*% fit$coef[1:ncol(X)])
+  #generate samples for each i
+  
+  error_out = F
+  q = ncol(Z) / d
+  gibbs_accept_rate = matrix(NA, nrow = d, ncol = q)
+  
+  if(gibbs == F){ # Rejection sampling
+    
+    for(i in 1:d){
+      
+      if(error_out){
+        next
+      }
+      
+      select = group == i
+      index = seq(i, ncol(Z), by = d)
+      ## new code to limit to non-zero z, skipping elements of q where diag(sigma) are 0
+      index = index[which(diag(cov) != 0)]
+      if(length(index) == 0) next
+      
+      draws = sample_mc_inner(matrix(fitted_mat[select], ncol = 1, nrow = sum(select)), 
+                              matrix(Z[select,index],ncol = length(index), nrow = sum(select)), 
+                              y[select], tau[select], nMC, trace)
+      
+      # If sample_mc_inner did not error out (i.e. have too small an acceptance rate), then continue
+      if(nrow(draws) == nMC){
+        u0[,index] = draws
+      }else{ # If too small an acceptance rate
+        cat("switched to gibbs sampling (single round) \n")
+        error_out = T
+        next
+      }
+      
+    }
+    
+    # If at any point the sample_mc_inner errored out due to too low of acceptance rate, then 
+    # switch to gibbs sampling (redo entire u matrix as gibbs sampling)
+    if(error_out){
+      for(i in 1:d){
+        select = group == i
+        index = seq(i, ncol(Z), by = d)
+        ## new code to limit to non-zero z, skipping elements of q where diag(sigma) are 0
+        index = index[which(diag(cov) != 0)]
+        if(length(index) == 0) next
+        var_index = which(diag(cov) != 0)
+        
+        gibbs_list = sample_mc_inner_gibbs(matrix(fitted_mat[select], ncol = 1, nrow = sum(select)), 
+                                           matrix(Z[select,index],ncol = length(index), nrow = sum(select)),  
+                                           y[select], uhat[index], nMC, as.numeric((uold[nrow(uold),index, drop = FALSE])), 
+                                           proposal_var[var_index], batch, trace)
+        u0[,index] = gibbs_list$u
+        gibbs_accept_rate[i,] = matrix(gibbs_list$acc_rate, nrow = 1)
+        proposal_var[i,var_index] = gibbs_list$proposal_var
+        batch = gibbs_list$batch
+      }
+    }
+    
+  }else{ # Gibbs sampling
+    for(i in 1:d){
+      select = group == i
+      index = seq(i, ncol(Z), by = d)
+      ## new code to limit to non-zero z, skipping elements of q where diag(sigma) are 0
+      index = index[which(diag(cov) != 0)]
+      if(length(index) == 0) next
+      var_index = which(diag(cov) != 0)
+      
+      gibbs_list = sample_mc_inner_gibbs(matrix(fitted_mat[select], ncol = 1, nrow = sum(select)), 
+                                         matrix(Z[select,index],ncol = length(index), nrow = sum(select)),  
+                                         y[select], uhat[index], nMC, as.numeric((uold[nrow(uold),index, drop = FALSE])), 
+                                         proposal_var[var_index], batch, trace)
+      u0[,index] = gibbs_list$u
+      gibbs_accept_rate[i,] = matrix(gibbs_list$acc_rate, nrow = 1)
+      proposal_var[i,var_index] = gibbs_list$proposal_var
+      batch = gibbs_list$batch
+    }
+  }
+  # for each i, rbind the nMC samples together to make n*nMC x d matrix (d = dim(Z))
+  
+  if(gibbs == F){
+    cat("switch from rejection to gibbs: ", error_out, "\n")
+  }
+  
+  # switch: variable indicating if switched from rejection sampling to Metropolis-within-Gibbs sampling
+  if(!anyNA(gibbs_accept_rate)){
+    return(list(u0 = u0, gibbs_accept_rate = gibbs_accept_rate, 
+                switch = error_out, proposal_var = proposal_var))
+  }else{
+    return(list(u0 = u0, switch = error_out, proposal_var = proposal_var))
   }
   
 }
--- src/sample_mc_inner_gibbs.cpp
+++ src/sample_mc_inner_gibbs.cpp
@@ -6,6 +6,7 @@ using namespace Rcpp;
 
 // Original: NumericMatrix sample_mc_inner_gibbs(...)
 
+// No adaptive change in proposal variation
 // [[Rcpp::export]]
 List sample_mc_inner_gibbs(arma::mat f, // matrix
                           arma::mat z, // matrix
@@ -53,7 +54,7 @@ List sample_mc_inner_gibbs(arma::mat f, // matrix
   for(i = 0; i < q; i++){
     e(i) = uold(i);
     index2(i) = 0.0;
-    var(i) = 1.5;
+    var(i) = 1.0;
   }
   
   //iteratively update e
@@ -100,6 +101,8 @@ List sample_mc_inner_gibbs(arma::mat f, // matrix
     }
     index++;
     
+    
+    
   }
   
   // Info for acceptance rate:
@@ -113,5 +116,138 @@ List sample_mc_inner_gibbs(arma::mat f, // matrix
   
   return(List::create(Named("u") = wrap(out), Named("acc_rate") = acc_rate));
   // return(wrap(out));
+  
+}
+
+// Adaptive change in proposal variation
+// [[Rcpp::export]]
+List sample_mc_inner_gibbs2(arma::mat f, // matrix
+                           arma::mat z, // matrix
+                           arma::vec y, // vector
+                           arma::vec t, // vector
+                           int NMC, // integer
+                           arma::vec u0, //matrix
+                           arma::vec proposal_var, // vector
+                           double batch,
+                           int trace){ // integer
+  
+  arma::mat fitted = f;
+  arma::mat Z=z;
+  arma::vec Y=y;
+  arma::vec tau =t;
+  int nMC = NMC; 
+  arma::vec uold=u0;
+  
+  int q = Z.n_cols;
+  int n = Z.n_rows;
+  
+  int i = 0;
+  int j = 0; 
+  // int k = 0; 
+  int l = 0; 
+  int naccept = 0;
+  int index = 0;
+  // int batch = 1;
+  double sum = 0;
+  double sumn = 0;
+  // double stao = 0;
+  double w = 0;
+  double ep = 0; 
+  double e0 = 0;
+  double delta = 0;
+  double increment = 0;
+  double batch_length = 50.0;
+  arma::mat out(nMC, q);
+  arma::vec e(q);
+  arma::vec rate(q);
+  arma::vec etae(n);
+  arma::vec etaen(n);
+  arma::vec index2(q);
+  arma::vec var = proposal_var; // Initially, proposal_var = 1.0 for each variable
+  arma::vec acc_rate(q);
+  
+  RNGScope scope;
+  
+  //initialize e 
+  for(i = 0; i < q; i++){
+    e(i) = uold(i);
+    index2(i) = 0.0;
+  }
+  
+  //iteratively update e
+  while(naccept < nMC){
+    
+    for(j = 0;j < q; j++){
+      
+      // calculate etae
+      etae = fitted + Z*e;
+      
+      // save current value of e(i)
+      e0 = e(j);
+      
+      // generate w
+      w = log(R::runif(0.0,1.0));
+      
+      // generate proposal e
+      ep = R::rnorm(0.0, var(j));
+      e(j) = ep;
+      
+      // calculate updated etae
+      etaen = fitted + Z*e;
+      
+      // calculate ratio
+      sum = sumn = 0;
+      for(l = 0; l < n; l++){
+        sum = sum + R::dbinom(Y(l), 1.0, exp(etae(l))/(1+exp(etae(l))), 1);
+        sumn = sumn + R::dbinom(Y(l), 1.0, exp(etaen(l))/(1+exp(etaen(l))), 1);
+      }
+      
+      sum = sum + R::dnorm4(e0, 0.0, 1.0, 1) + R::dnorm4(ep, 0.0, var(j), 1) ;
+      sumn = sumn + R::dnorm4(ep, 0.0, 1.0, 1) + R::dnorm4(e0, 0.0, var(j), 1)  ;
+      
+      // check for acceptance
+      if(w < sumn - sum){
+        // ep left in e(i)
+        index2(j) = index2(j) + 1.0;
+      }else{
+        e(j) = e0;
+      }
+      //  }
+    }
+    
+    if(index == 1+5*naccept){
+      out.row(naccept) = e.t();
+      naccept++;
+    }
+    index++;
+    
+    // Updated proposal variance
+    if(index % (int)batch_length == 0){
+      // Update batch information 
+      batch = batch + batch_length;
+      // Determine acceptance rate for latest batch
+      acc_rate(j) = index2(j) / batch_length;
+      // Update proposal variance (separate for each variable)
+      increment = sqrt(1 / batch);
+      if(increment < 0.01){
+        delta = increment;
+      }else{
+        delta = 0.01;
+      }
+      if(acc_rate(j) > 0.5){
+        var(j) = var(j) * exp(-2*delta);
+      }else if(acc_rate(j) < 0.4){
+        var(j) = var(j) * exp(2*delta);
+      }
+      
+      // Re-set index2 - new acceptance rate for next batch
+      index2(j) = 0.0;
+    }
+    
+  }
+  
+  return(List::create(Named("u") = wrap(out), Named("acc_rate") = acc_rate,
+                      Named("proposal_var") = var));
+  
   
 }
