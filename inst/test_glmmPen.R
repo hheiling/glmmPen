69ef3093d95f9916c46055b036aa71e3f64b6544
hheiling <hheiling@live.unc.edu>
2020-06-03 15:41:59 -0400

    Personal test code: adjustments

--- inst/test_glmmPen.R
+++ inst/test_glmmPen.R
@@ -6,6 +6,8 @@
 ## Input formula: y ~ X + (X | group) where input just vectors and matrices
 ## Check both covar = "unstructured" and covar = "independent"
 
+library(glmmPen)
+
 dat = sim.data2(n = 500, ptot = 2, pnonzero = 2, nstudies = 5, sd_raneff = 1.0, family = 'binomial',
                       slopes = T, seed = 2020, imbalance = 1, pnonzerovar = 0, beta = c(0, 2, 2))
 
@@ -13,11 +15,12 @@ y = dat$y
 X = dat$X[,-1]
 grp = dat$group
 out1 = glmmPen(formula = y ~ X + (X | grp), family = "binomial", 
-               optim_options = optimControl(nMC_start = 200, nMC_max = 1000, maxitEM = 10, fit_type=2), 
+               optim_options = optimControl(nMC_start = 200, nMC_max = 1000, maxitEM = 10, fit_type=1), 
                tuning_options = lambdaControl())
 
 out1B = glmmPen(formula = y ~ X + (X | grp), family = "binomial", 
-               optim_options = optimControl(nMC_start = 200, nMC_max = 1000, maxitEM = 10, covar = "independent", fit_type=2), 
+               optim_options = optimControl(nMC_start = 200, nMC_max = 1000, maxitEM = 10, 
+                                            covar = "independent", fit_type=2), 
                tuning_options = lambdaControl())
 
 head(predict(out1B, type = "response"))
